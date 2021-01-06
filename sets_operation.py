75b60457730eb152da716865c4f2c889803d4b0b
faris <faris.shi84@gmail.com>
2021-01-06 02:05:45 -0500

    add comments

--- sets_operation.py
+++ sets_operation.py
@@ -8,17 +8,23 @@ baseball = {"Jodi", "Carmen", "Aida", "Alicia"}
 basketball = {"Eva", "Carmen", "Alicia", "Sarah"}
 
 
+#intersection
 #Display players that play both baseball and basketball
 print(baseball & basketball)
 
+
+#combination
 #Display players that play either baseball or basketball
 print(baseball | basketball)
 
+#difference
 #Display players that play baseball, but not basketball
 print(baseball - basketball)
 
+#difference
 # Display players that play basketball, but not baseball
 print(basketball - baseball)
+
 
 # Display players that play one sport, but not both
 print(basketball ^ baseball)
